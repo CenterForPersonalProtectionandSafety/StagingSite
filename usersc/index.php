8e2efeeea5295ef143d4995b6b723054ad388efc
marnold22 <minimearnold@gmail.com>
2019-07-15 14:49:22 -0700

    Force change password test
    Function to check if user has changed thier defualt pasword.

--- usersc/index.php
+++ usersc/index.php
@@ -7,7 +7,11 @@ If not logged in, redirect to login page.
 <?php
 require_once '../users/init.php';
 if(isset($user) && $user->isLoggedIn()){
-  Redirect::to($us_url_root.'index.php');
+  if ($user->data()->updated_pass_status == 0) {
+    Redirect::to($us_url_root.'usersc/user_settings.php');
+  }else {
+    Redirect::to($us_url_root.'index.php');
+  }
 }else{
   Redirect::to($us_url_root.'usersc/login.php');
 }
