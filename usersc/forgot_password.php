54e84a04c44200e70144039d82a98c8b67e251e8
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-09-16 19:28:21 -0700

    Added path to PHPMailer()

--- usersc/forgot_password.php
+++ usersc/forgot_password.php
@@ -23,6 +23,13 @@ require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
 
 if (!securePage($_SERVER['PHP_SELF'])){die();}
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
 if(ipCheckBan()){Redirect::to($us_url_root.'usersc/scripts/banned.php');die();}
 $error_message = null;
 $errors = array();
