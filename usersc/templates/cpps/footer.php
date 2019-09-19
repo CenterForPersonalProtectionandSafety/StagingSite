fa354b0c24fcdb365fdc6d08a4da363c2bfc0bc5
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-09-18 18:13:58 -0700

    Cleaned up template footer
    Broke up inline scripts to seperate js files and linked in footer.

--- usersc/templates/cpps/footer.php
+++ usersc/templates/cpps/footer.php
@@ -5,26 +5,16 @@ require_once $abs_us_root . $us_url_root . 'users/includes/page_footer.php';
 
 ?>
 
-<script type="text/javascript">
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-});
-</script>
-<script>
-var $hamburger = $(".hamburger");
-$hamburger.on("click", function(e) {
-  $hamburger.toggleClass("is-active");
-  // Do something else, like open/close menu
-});
-</script>
 
-<div class="container-fluid">
-  <div class="row">
-    <footer class="text-center footer">
-      <br>
-      &copy; <?php echo date("Y"); ?> <?=$settings->copyright; ?>
-      <br><br>
-    </footer>
-  </div>
-</div>
+<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/tooltip.js"></script>
+<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/hamburger.js"></script>
+<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/custom.js"></script>
+
+
+<?php if($user->isLoggedIn()){ //anyone is logged in?>
+  <footer class="login-footer">&copy; <?php echo date("Y"); ?> <?=$settings->copyright; ?></footer>
+<?php } else{ ?>
+  <footer class="logout-footer">&copy; <?php echo date("Y"); ?> <?=$settings->copyright; ?></footer>
+<?php } ?>
+
 <?php require_once($abs_us_root.$us_url_root.'users/includes/html_footer.php');?>
