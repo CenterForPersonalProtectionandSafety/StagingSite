9816fb9da7bce5df443fcb5b85f97362a88b20db
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-09-20 20:42:36 -0700

    Fixed javascript errors
    Moved jquery, ajax, and other library calls to template footer

--- usersc/templates/cpps/header.php
+++ usersc/templates/cpps/header.php
@@ -13,6 +13,7 @@
 <!-- Custom CSS -->
 <link rel="stylesheet" href="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/css/custom.css">
 <link rel="stylesheet" href="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/css/card-flip.css">
+<link rel="stylesheet" href="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/css/hamburgers.min.css">
 
 
 <style media="screen">
@@ -21,14 +22,6 @@
   }
 </style>
 
-
-<!-- jQuery & Ajax  -->
-<script src="https://code.jquery.com/jquery-3.4.0.min.js" integrity="sha256-BJeo0qm959uMBGb65z40ejJYGSgR7REI4+CW1fNKwOg=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-<script src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/bootstrap.min.js" type="text/javascript"></script>
-<link href="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/css/hamburgers.min.css" rel="stylesheet">
 
 <!-- Optional bootstrap corrections -->
 <?php //require_once $abs_us_root.$us_url_root.'usersc/includes/bootstrap_corrections.php'; ?>
