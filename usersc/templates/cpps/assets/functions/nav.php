5bcf4cca5df5a8215191dbd16f93d3f68712b4e1
Michael Arnold <michaelarnold@Michaels-MacBook-Pro.local>
2019-10-08 14:46:10 -0700

    testing navbar

--- usersc/templates/cpps/assets/functions/nav.php
+++ usersc/templates/cpps/assets/functions/nav.php
@@ -1,5 +1,5 @@
 <?php if($user->isLoggedIn()){ //anyone is logged in?>
-	<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top">
 		<div class="container">
 			<a class="navbar-brand" href="<?=$us_url_root?>"><img class="img-responsive" src="<?=$us_url_root?>usersc/images/logo.png" alt="" /></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
