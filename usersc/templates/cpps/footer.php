2380d35dc6f613d8b1189400959a07bb1ed878e9
Marnold22 <minimearnodl@gmail.com>
2019-12-16 11:31:10 -0800

    Needed to add .min to js files in footer.

--- usersc/templates/cpps/footer.php
+++ usersc/templates/cpps/footer.php
@@ -5,14 +5,13 @@ require_once $abs_us_root . $us_url_root . 'users/includes/page_footer.php';
 
 ?>
 
-<!-- jQuery & Ajax  -->
 <script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/popper.min.js"></script>
 <script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/bootstrap.min.js"></script>
 
-<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/custom.js"></script>
-<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/tooltip.js"></script>
-<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/hamburger.js"></script>
+<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/custom.min.js"></script>
+<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/tooltip.min.js"></script>
+<script type="text/javascript" src="<?=$us_url_root?>usersc/templates/<?=$settings->template?>/assets/js/hamburger.min.js"></script>
 
 <?php if($user->isLoggedIn()){ //anyone is logged in?>
   <footer class="login-footer">&copy; <?php echo date("Y"); ?> <?=$settings->copyright; ?></footer>
